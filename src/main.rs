86a5d35414b06a4da494f4a40a80ee959ccd574b
Lavi <lavifb@gmail.com>
2018-10-14 19:00:16 -0400

    working simple TODO parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+extern crate regex;
+
+use std::env;
+use std::fs;
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+
+	let mut files = Vec::new();
+
+    // For now we will just open files given in args
+    // TODO: get list of tracked files
+    for arg in env::args().skip(1) {
+	    println!("{}\n", arg);
+	    files.push(arg)
+	}
+
+    // open each file and look for TODO comments
+	for file in files.iter() {
+		// TODO: look at file extension to figure out how to parse
+		let file_contents = fs::read_to_string(file).unwrap();
+
+		// TODO: add custom TODO keywords
+		// TODO: record line numbers
+		// TODO: use a better regex to find TODOs
+		// TODO: add more regexs for other languages/patterns
+		let re = Regex::new(r"(?m)^(\s*)// TODO:(.*)$").unwrap();
+
+		// TODO: store TODOs in buffer before printing for other uses
+		for todo in re.captures_iter(&file_contents) {
+			println!("{}", &todo[0].trim());
+		}
+	}
 }
