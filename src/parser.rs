296251ea9782cd2aeea58bba4be5dcfba7563799
Lavi <lavifb@gmail.com>
2018-10-15 16:14:10 -0400

    found TODOs are now stored in Todo struct

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,7 @@
 // Module for finding TODOs in files
 
 use regex::Regex;
+use std::fmt;
 
 /// A struct holding the TODO and all the needed meta-information for it.
 pub struct Todo {
@@ -9,12 +10,32 @@ pub struct Todo {
 	content: String,
 }
 
+impl Todo {
+	fn new(line: usize, todo_type_str: &str, content_str: &str) -> Todo {
+		
+		let todo_type = todo_type_str.to_string();
+		let content = content_str.to_string();
+
+		Todo {
+			line,
+			todo_type,
+			content,
+		}
+	}
+}
+
+impl fmt::Display for Todo {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}\t{}\t{}", self.line, self.todo_type, self.content)
+    }
+}
+
+
 /// Creates a list of TODOs found in content
 // TODO: return list of TODOs
 pub fn find_todos(content: &str) {
 	
 	// TODO: add custom TODO keywords
-	// TODO: record line numbers
 	// TODO: use a better regex to find TODOs
 	// TODO: add more regexs for other languages/patterns
 	let re = Regex::new(r"^\s*// TODO:(.*)$").unwrap();
@@ -22,12 +43,12 @@ pub fn find_todos(content: &str) {
 	let mut line_num: usize = 0;
 
 	for line in content.lines() {
-
 		line_num += 1;
 
-		// TODO: store TODOs in Todo struct
-		for todo in re.captures_iter(line) {
-			println!("{}\tTODO\t{}", line_num, &todo[1].trim());
+		for todo_content in re.captures_iter(line) {
+			let todo = Todo::new(line_num, "TODO", todo_content[1].trim());
+
+			println!("{}", todo);
 		}
 	}
 }
