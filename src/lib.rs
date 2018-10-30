33ce9e73371f1f82206d38d9a807ea0f9112c5ad
Lavi <lavifb@gmail.com>
2018-10-30 18:58:53 -0400

    implemented TodoR::open_todos()

--- src/lib.rs
+++ src/lib.rs
@@ -61,14 +61,23 @@ struct TodoFile {
 	todos: Vec<Todo>,
 }
 
-/// TODO finder that stores all of the found TODOs on a per-file basis.
+impl TodoFile {
+	fn new(filename: &str) -> TodoFile {
+		TodoFile {
+			filename: filename.to_string(),
+			todos: Vec::with_capacity(0), // do not allocate because it will be replaced
+		}
+	}
+}
+
+/// Parser for finding TODOs in comments and storing them on a per-file basis.
 pub struct TodoR {
 	pub config: TodoRConfig,
 	todo_files: Vec<TodoFile>,
 }
 
 impl TodoR {
-	/// Creates new TodoR struct with provided configuration.
+	/// Creates new TodoR that looks for provided keywords.
 	pub fn new<T: AsRef<str>>(todo_words: &[T]) -> TodoR {
 		TodoR {
 			config: TodoRConfig::new(todo_words),
@@ -78,10 +87,23 @@ impl TodoR {
 
 	/// Opens file at given filename and process it by finding all its TODOs.
 	pub fn open_todos(&mut self, filename: &str) -> Result<()> {
-		// TODO: implement
-		unimplemented!();
 
+		let mut todo_file = TodoFile::new(filename);
+		let file_ext = filename.rsplitn(2, '.').next().unwrap();
+		let mut file = File::open(filename)?;
 
+		// check the file is not a directory
+		if file.metadata()?.is_dir() {
+			return Err(format!("'{}' is a directory.", filename).into());
+		}
+
+		let mut file_contents = String::new();
+		// TODO: Maybe use buffer in case file is very large
+		file.read_to_string(&mut file_contents)?;
+		todo_file.todos = find_todos(&file_contents, file_ext, &self.config.todo_words);
+
+		self.todo_files.push(todo_file);
+		Ok(())
 	}
 
 	/// Prints TODOs to stdout.
