a2c673b309de52ba50a4250a4b421d221e0dc150
Lavi <lavifb@gmail.com>
2018-11-13 12:30:12 -0500

    fixed bench to go with type changes

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,6 +2,7 @@
 extern crate criterion;
 extern crate todo_r;
 
+use std::path::Path;
 use criterion::Criterion;
 use todo_r::TodoR;
 use todo_r::errors::eprint_error;
@@ -10,7 +11,7 @@ fn bench_jquery(c: &mut Criterion) {
     c.bench_function("jquery", |b| b.iter(|| {
 		let todo_words = ["TODO", "FIXME"];
     	let mut todor = TodoR::new(&todo_words);
-    	todor.open_todos("benches/inputs/jquery-3.3.1.js").unwrap_or_else(|err| eprint_error(&err));
+    	todor.open_todos(Path::new("benches/inputs/jquery-3.3.1.js")).unwrap_or_else(|err| eprint_error(&err));
     }));
 }
 
