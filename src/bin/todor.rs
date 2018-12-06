5ec32f35abe904cbe88d1953e3bfd8e220de42fc
Lavi <lavifb@gmail.com>
2018-12-06 17:07:17 -0500

    small fix

--- src/bin/todor.rs
+++ src/bin/todor.rs
@@ -95,11 +95,11 @@ fn run(matches: &ArgMatches) -> Result<i32, Error> {
 			}
 
 			let mut curr_dir = current_dir()?;
+			let mut ignore_builder = OverrideBuilder::new(&curr_dir);
 			curr_dir.push(".todor");
 			let mut relative_path = PathBuf::from(".");
-			let mut found_walker_root = false;
 			let mut walk_builder = WalkBuilder::new(&relative_path);
-			let mut ignore_builder = OverrideBuilder::new(&curr_dir);
+			let mut found_walker_root = false;
 
 			for path in curr_dir.ancestors() {
 				let ignore_path = relative_path
