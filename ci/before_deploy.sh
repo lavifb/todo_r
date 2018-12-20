90121f96be424674b9e2b8b819a65cd39081fe0c
Lavi <lavifb@gmail.com>
2018-12-20 14:47:34 -0500

    maybe fixed before_deploy

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -27,10 +27,10 @@ main() {
 
     # copy completions to stage
     mkdir $stage/complete
-    cp "target/$TARGET/release/build/$PROJECT_NAME"-*/out/"$PROJECT_NAME.bash" $stage/complete/${PROJECT_NAME}.bash-completion
-    cp "target/$TARGET/release/build/$PROJECT_NAME"-*/out/"$PROJECT_NAME.fish" $stage/complete/
-    cp "target/$TARGET/release/build/$PROJECT_NAME"-*/out/"_$PROJECT_NAME" $stage/complete/
-    cp "target/$TARGET/release/build/$PROJECT_NAME"-*/out/"_$PROJECT_NAME.ps1" $stage/complete/
+    cp target/"$TARGET"/release/build/"$PROJECT_NAME"-*/out/"$PROJECT_NAME".bash $stage/complete/${PROJECT_NAME}.bash-completion
+    cp target/"$TARGET"/release/build/"$PROJECT_NAME"-*/out/"$PROJECT_NAME".fish $stage/complete/
+    cp target/"$TARGET"/release/build/"$PROJECT_NAME"-*/out/_"$PROJECT_NAME" $stage/complete/
+    cp target/"$TARGET"/release/build/"$PROJECT_NAME"-*/out/_"$PROJECT_NAME".ps1 $stage/complete/
 
     cd $stage
     tar czf $src/$PROJECT_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
