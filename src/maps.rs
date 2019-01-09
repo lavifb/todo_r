3e792bb37e2218d3c6be2edf9c9ade2ef9279bdd
Lavi <lavifb@gmail.com>
2019-01-09 14:50:05 -0500

    added some documentation for FallbackHashMap

--- src/maps.rs
+++ src/maps.rs
@@ -4,6 +4,10 @@ use fnv::FnvHashMap;
 use std::borrow::Borrow;
 use std::hash::Hash;
 
+/// FallbackHashMap is a Hashmap that yields a fallback value for `get(k)` if `k` has not been
+/// inserted.
+///
+/// FallbackHashMap uses `fnv::FnvHashMap` as its hasher.
 #[derive(Debug, Clone)]
 pub struct FallbackHashMap<K: Hash + Eq, V> {
     map: FnvHashMap<K, V>,
@@ -11,6 +15,7 @@ pub struct FallbackHashMap<K: Hash + Eq, V> {
 }
 
 impl<K: Hash + Eq, V> FallbackHashMap<K, V> {
+    /// creates new FallbackHashMap
     pub fn new(fallback_value: V) -> FallbackHashMap<K, V> {
         FallbackHashMap {
             map: FnvHashMap::default(),
@@ -18,10 +23,12 @@ impl<K: Hash + Eq, V> FallbackHashMap<K, V> {
         }
     }
 
+    /// Inserts value `v` into FallbackHashMap with key `k`
     pub fn insert(&mut self, k: K, v: V) -> Option<V> {
         self.map.insert(k, v)
     }
 
+    /// Gets the value for key `k`. If `k` has not been inserted, fallback value is returned
     pub fn get<Q: ?Sized>(&self, k: &Q) -> &V
     where
         K: Borrow<Q>,
