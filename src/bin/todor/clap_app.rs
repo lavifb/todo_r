290aa7defd5a4934ce4bd81b9db14a8c53d8366b
Lavi <lavifb@gmail.com>
2019-01-10 18:25:56 -0500

    finished converting clap macro

--- src/bin/todor/clap_app.rs
+++ src/bin/todor/clap_app.rs
@@ -54,7 +54,53 @@ pub fn build_cli() -> App<'static, 'static> {
                 .long("ignore")
                 .takes_value(true)
                 .multiple(true)
-                .help("Paths to be ignored."),
+                .help("Files to be ignored."),
+        )
+        .arg(
+            Arg::with_name("VERBOSE")
+                .short("v")
+                .long("verbose")
+                .help("Provide verbose output."),
+        )
+        .arg(
+            Arg::with_name("CHECK")
+                .long("check")
+                .help("Exits nicely only if no TODO comments are found."),
+        )
+        .arg(
+            Arg::with_name("FORMAT")
+                .short("f")
+                .long("format")
+                .takes_value(true)
+                .possible_values(&[
+                    "json",
+                    "prettyjson",
+                    "markdown",
+                    "usermarkdown",
+                    "csv",
+                    "default",
+                ])
+                .help("Outputs in specified format."),
+        )
+        .arg(
+            Arg::with_name("DELETE_MODE")
+                .short("d")
+                .long("delete")
+                .conflicts_with("FORMAT")
+                .help("Interactive delete mode."),
+        )
+        .arg(
+            Arg::with_name("EXT")
+                .short("e")
+                .long("ext")
+                .takes_value(true)
+                .conflicts_with("DELETE_MODE")
+                .help("Reads piped content as if it has the provided extention."),
+        )
+        .subcommand(
+            App::new("init")
+                .about("Creates .todor config file and defines todor workspace.")
+                .author("Lavi Blumberg <lavifb@gmail.com>"),
         );
 
     clap_app!(todo_r =>
