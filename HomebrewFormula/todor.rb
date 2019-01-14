e3704b274f90d3af67162389abe339683e8b3b34
Lavi <lavifb@gmail.com>
2019-01-14 15:10:34 -0500

    upgraded brew formula

--- HomebrewFormula/todor.rb
+++ HomebrewFormula/todor.rb
@@ -1,14 +1,14 @@
 class Todor < Formula
-  version '0.7.0'
+  version '0.7.1'
   desc "Find all your TODO notes with one command!"
   homepage "https://github.com/lavifb/todo_r"
 
   if OS.mac?
-      url "https://github.com/lavifb/todo_r/releases/download/v0.7.0/todor-v0.7.0-x86_64-apple-darwin.tar.gz"
-      sha256 "43187106a9377bce87c07813af75346494faa5f4d54e0ea45c9d02da2ff3b46e"
+      url "https://github.com/lavifb/todo_r/releases/download/v0.7.1/todor-v0.7.1-x86_64-apple-darwin.tar.gz"
+      sha256 "70375c189ec899232efafee0449d3c21b05cc61eb81ad570f62a3485781b89ef"
   elsif OS.linux?
-      url "https://github.com/lavifb/todo_r/releases/download/v0.7.0/todor-v0.7.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "22b08622f9b580ecade36775bcace9a009b10c8dcfefd611bdddf3987ed255a7"
+      url "https://github.com/lavifb/todo_r/releases/download/v0.7.1/todor-v0.7.1-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "c390be32d6e33cd06baf1b9c0253f904f4ecd6b413dba8ea3e32b73799cf8406"
   end
 
   conflicts_with "todor"
